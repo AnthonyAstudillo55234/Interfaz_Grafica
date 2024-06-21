64b1a8c9dabe84e643293549bcf486174f86bc80
Anthony <anthony.astudillo@epn.edu.ec>
2024-06-21 12:39:22 -0500

    CAMBIO TRES

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and Welcome");
+        /*System.out.println("Hello and Welcome");
 
         String primerNumero= JOptionPane.showInputDialog("Introduzca el primer entero");
         String segundoNumero= JOptionPane.showInputDialog("Introduzca el segundo entero");
@@ -16,6 +17,12 @@ public class Main {
         int multiplicacion=numero1*numero2;
         int division=numero1/numero2;
 
-        JOptionPane.showMessageDialog(null,"El resultado de la suma es: " +suma+"\nEl resultado de la resta es: "+"\nEl resultado de la mult es: "+multiplicacion+"\nEl resultado de la division es: "+division, "Suma de Enteros",JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null,"El resultado de la suma es: " +suma+"\nEl resultado de la resta es: "+"\nEl resultado de la mult es: "+multiplicacion+"\nEl resultado de la division es: "+division, "Suma de Enteros",JOptionPane.PLAIN_MESSAGE);*/
+
+        JFrame frame = new JFrame("Mi aplicación");
+        frame.setContentPane(new form1().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
--- src/form1.java
+++ src/form1.java
@@ -1,6 +1,60 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class form1 {
-    private JPanel panel1;
-    private JButton button1;
+    public JPanel mainPanel;
+    private JLabel nombreLbl;
+    private JButton sumarButton;
+    private JTextField num1;
+    private JButton multiplicarButton;
+    private JTextField num2;
+    private JButton restarButton;
+    private JButton dividirButton;
+
+
+    public form1() {
+        sumarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                float numero1 = Float.parseFloat(num1.getText());
+                float numero2 = Float.parseFloat(num2.getText());
+                float resultado = numero1 + numero2;
+                nombreLbl.setText(String.valueOf("Resultado: "+resultado));
+            }
+        });
+        multiplicarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                float numero1 = Float.parseFloat(num1.getText());
+                float numero2 = Float.parseFloat(num2.getText());
+                float resultado = numero1 * numero2;
+                nombreLbl.setText(String.valueOf("Resultado: "+resultado));
+            }
+        });
+        restarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                float numero1 = Float.parseFloat(num1.getText());
+                float numero2 = Float.parseFloat(num2.getText());
+                float resultado = numero1 - numero2;
+                nombreLbl.setText(String.valueOf("Resultado: "+resultado));
+            }
+        });
+        dividirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                float numero1 = Float.parseFloat(num1.getText());
+                float numero2 = Float.parseFloat(num2.getText());
+                float resultado = numero1 / numero2;
+                if (numero2==0){
+                    nombreLbl.setText(String.valueOf("Resultado: no se puede dividir para 0"));
+                }else {
+                    nombreLbl.setText(String.valueOf("Resultado: "+resultado));
+                }
+            }
+        });
+    }
 }
+
+
